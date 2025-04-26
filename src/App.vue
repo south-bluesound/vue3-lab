6a5dd5123cb306c91c9cf68a2ebd09b81729219e
minami.kingo <minami.kingo@digitalvalue.co.jp>
2025-04-26 20:36:47 +0900

    :sparkles: v-ifで表示・非表示を切り替える

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,8 @@ const items = ref([
     description:
       '刻んだ野菜をアボカドと混ぜてディップに。こんがり焼いたバゲットとお召し上がりください。',
     price: 480,
-    image: '/images/item1.jpg'
+    image: '/images/item1.jpg',
+    soldOut: false
   },
   {
     id: 2,
@@ -15,7 +16,8 @@ const items = ref([
     description:
       '子供のころに食べたかった、あのホットケーキを再現しました。素朴でどこか懐かしい味をどうぞ。',
     price: 1180,
-    image: '/images/item2.jpg'
+    image: '/images/item2.jpg',
+    soldOut: false
   },
   {
     id: 3,
@@ -23,7 +25,8 @@ const items = ref([
     description:
       'ロサンゼルス生まれのスパークリングウォーター。ノンカロリー、ノンアルコールの新感覚飲料です。',
     price: 320,
-    image: '/images/item3.jpg'
+    image: '/images/item3.jpg',
+    soldOut: true
   },
   {
     id: 4,
@@ -31,7 +34,8 @@ const items = ref([
     description:
       'イタリア産チーズをたっぷりかけたアツアツのフレンチフライ。みんな大好きな一品です。',
     price: 670,
-    image: '/images/item4.jpg'
+    image: '/images/item4.jpg',
+    soldOut: false
   }
 ])
 </script>
@@ -47,7 +51,7 @@ const items = ref([
     <template
       v-for="item in items"
       :key="item.id">
-      <div class="item">
+      <div v-if="!item.soldOut" class="item">
         <div class="thumbnail">
           <img
             :src="item.image"
@@ -59,6 +63,7 @@ const items = ref([
           <span>￥<span class="price">{{ item.price }}</span></span>
         </div>
       </div>
+      <div v-else>売り切れです！</div>
     </template>
   </main>
 </template>
